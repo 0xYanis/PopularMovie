d99bdb06176f78ced0cc37ba41ba1ee4fef5dca7
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-02-10 15:08:38 +0300

    Configured display of Header in Home Page

--- PopularMovie/Controller/Home/HeaderHorizontalController.swift
+++ PopularMovie/Controller/Home/HeaderHorizontalController.swift
@@ -9,4 +9,39 @@ import UIKit
 
 class HeaderHorizontalController: HorizontalSnappingController {
 	
+	
+	fileprivate let cellId = "cellId"
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+		collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+		collectionView.showsHorizontalScrollIndicator = false
+	}
+	
+	
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 5
+	}
+	
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+		cell.backgroundColor = .purple
+		return cell
+	}
+}
+
+// MARK: - DelegateFlowLayout protocol
+extension HeaderHorizontalController: UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return .init(top: 0, left: 16, bottom: 0, right: 16)
+	}
+	
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return .init(width: view.frame.width - 240, height: view.frame.height)
+	}
 }
