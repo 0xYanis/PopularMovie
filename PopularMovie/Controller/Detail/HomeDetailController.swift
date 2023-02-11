4fb3bdf4c0ffa5462738a6d0e90580d5448d66cb
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-02-11 18:40:52 +0300

    Configured detail view

--- PopularMovie/Controller/Detail/HomeDetailController.swift
+++ PopularMovie/Controller/Detail/HomeDetailController.swift
@@ -11,7 +11,11 @@ class HomeDetailController: BaseController {
 	
 	
 	fileprivate let filmId: Int
+	fileprivate let posterId = "posterId"
 	
+	
+	fileprivate var popularMovies: Page?
+	fileprivate var tvGroup: TVGroup?
 	
 	
 	init(filmId: Int) {
@@ -30,5 +34,18 @@ class HomeDetailController: BaseController {
 		
 		
 		view.backgroundColor = .red
+		collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: posterId)
+	}
+	
+	
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: posterId, for: indexPath)
+		cell.backgroundColor = .red
+		return cell
 	}
 }
